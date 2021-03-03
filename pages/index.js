6080d5103ebff6e9a6f558d46800d1cc4f57d4b3
nolangendron <nolanglengendron@gmail.com>
2021-03-03 03:16:45 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+import products from '../products.json'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Space Jelly Shop</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          The Jelly Shop
+        </h1>
+
+        <ul className={styles.grid}>
+          {products.map((product) => {
+            const {id, title, image, description, price} = product
+            return (
+              <li key={id} className={styles.card}>
+                <a href="#">
+                  <img src={image} alt={title} />
+                  <h3>{title}</h3>
+                  <p>${price}</p>
+                  <p>{description}</p>
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
