9b66c4c8354944104071b9ad9d57ab7a080e4661
nolan <nolanglengendron@gmail.com>
2021-03-03 06:52:04 -0800

    Add cart

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,62 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 import products from '../products.json'
+import { initiateCheckout } from '../lib/payments'
+
+const defaultCart = {
+  products: {}
+}
 
 export default function Home() {
+
+  const [cart, updateCart] = useState(defaultCart)
+
+  const cartItems = Object.keys(cart.products).map(key => {
+    const product = products.find(({ id }) => `${id}` === `${key}`)
+    return {
+      ...cart.products[key],
+      pricePerItem: product.price
+    }
+  })
+
+  const subtotal = cartItems.reduce((accumulator, { pricePerItem, quantity }) => {
+    return accumulator + (pricePerItem * quantity)
+  }, 0)
+
+  const totalItems = cartItems.reduce((accumulator, { quantity }) => {
+    return accumulator + quantity
+  }, 0)
+
+  const addToCart = ({ id } = {}) => {
+    updateCart(prev => {
+      let cartState = {...prev}
+
+      if (cartState.products[id]) {
+        cartState.products[id].quantity = cartState.products[id].quantity + 1
+      } else {
+        cartState.products[id] = {
+          id, 
+          quantity: 1
+        }
+      }
+      return cartState
+      })
+    }
+
+    const checkout = () => {
+      initiateCheckout({
+        lineItems: cartItems.map(item => {
+          return {
+            price: item.id,
+            quantity: item.quantity
+          }
+        })
+      })
+    }
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,6 +69,14 @@ export default function Home() {
           The Lorem Ipsum Shop
         </h1>
 
+        <p>
+          <strong>Items:</strong> {totalItems}
+          <br />
+          <strong>Total Cost:</strong> ${subtotal}
+          <br/>
+          <button className={styles.button} onClick={checkout}>Check Out</button>
+        </p>
+
         <ul className={styles.grid}>
           {products.map((product) => {
             const {id, title, image, description, price} = product
@@ -27,22 +88,19 @@ export default function Home() {
                   <p>${price}</p>
                   <p>{description}</p>
                 </a>
+                <button 
+                  className={styles.button}
+                  onClick={() => 
+                    addToCart({
+                      id
+                    })
+                }
+                >Add to Cart</button>
               </li>
             )
           })}
         </ul>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
